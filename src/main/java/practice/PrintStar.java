a305785260ea9f76516471cb513992a73fb533d5
mepmepkimo <lkjh855048>
2020-11-17 00:04:12 +0800

    java

---
+++ src/main/java/practice/PrintStar.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class PrintStar {
+
+    public static void main(String[] args) {
+        int line = 5;
+
+        for (int i = 1; i <= 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("0");
+
+
+            }
+            System.out.println();
+        }
+
+    }
+
+}
