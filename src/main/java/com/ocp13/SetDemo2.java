f7f03288e7f93e2ce49c564772914b39c6a28120
MB-study <MB-study@192.168.1.153>
2020-12-22 19:35:07 +0800

    java

---
+++ src/main/java/com/ocp13/SetDemo2.java
@@ -0,0 +1,24 @@
+package com.ocp13;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo2 {
+
+    public static void main(String[] args) {
+        Set set = new TreeSet();//LinkedHashSet();
+        //禁採539,1-39中取5個不重複的數字
+        //電腦選號
+        while (set.size() < 5) {
+            //產生一個1-39的隨機數
+            int n = new Random().nextInt(39) + 1;
+            set.add(n);
+            System.out.println("加入 ==>" + n);
+        }
+        System.out.println(set);
+    }
+
+}
