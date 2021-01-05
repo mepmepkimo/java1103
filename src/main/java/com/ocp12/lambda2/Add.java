2247ffe08e109883ff8cfcaf3075b070d3bbe894
MB-study <MB-study@192.168.1.153>
2021-01-05 21:41:56 +0800

    java

--- src/main/java/com/ocp12/lambda2/Add.java
+++ src/main/java/com/ocp12/lambda2/Add.java
@@ -1,13 +1,11 @@
+
 package com.ocp12.lambda2;
 
 @FunctionalInterface
 public interface Add {
-
-    int sum(int x, int y);
-
-    public int hashCode();//公用方法
-    default void copyright(){//default 有實作的方法
+    int sum(int x,int y,int z);
+    public int hashCode();    //公用方法<<<這行不要留也可以RUN
+    default void copyright(){   //default 有時做的方法<<<這行不要留也可以RUN
         
     }
-
 }
