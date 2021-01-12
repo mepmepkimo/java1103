b607fe60a9394f22169de30c9721054f920c40ea
MB-study <MB-study@192.168.1.153>
2021-01-12 21:47:05 +0800

    Merge origin/master
    Conflicts:
    	src/main/java/com/ocp15/GroupByDemo.java
    	src/main/java/com/ocp15/GroupByDemo2.java
    	src/main/java/com/poker/Game.java

--- src/main/java/com/pattern/observer/News.java
+++ src/main/java/com/pattern/observer/News.java
@@ -8,18 +8,18 @@ public class News implements Subject{
     private  List<Observer> observers = new ArrayList<>();//我的粉絲列表(人數)
 
     @Override
-    public void add(Observer obsever) {
+    public void add(Observer observer) {
         observers.add(observer);
     }
 
     @Override
-    public void remove(Observer obsever) {
+    public void remove(Observer observer) {
         observers.remove(observer);
     }
 
     @Override
     public void notifyOberver(String data) {
-        obsevers.stream().forEach(o->o.update(data));
+        observers.stream().forEach(o->o.update(data));
     }
     
     
