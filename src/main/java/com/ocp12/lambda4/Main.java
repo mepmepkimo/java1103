2edbb0fcfd55bc7e27f01be2c5a5e5a4e43896f4
mepmepkimo <lkjh855048>
2021-01-19 06:08:13 +0800

    Merge origin/master
    Conflicts:
    	src/main/java/practice2/ATM.java

--- src/main/java/com/ocp12/lambda4/Main.java
+++ src/main/java/com/ocp12/lambda4/Main.java
@@ -1,6 +1,8 @@
 
 package com.ocp12.lambda4;
 
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
         Print print1 = (x)-> System.out.println(x);
@@ -8,6 +10,11 @@ public class Main {
         
         print1.display(100);
         print2.display(100);
+        
+        //使用CONSUMER
+        // void accept(T t)
+        Consumer<String> consumer=(x)-> System.out.println(x+x);
+        consumer.accept("Ha");
     }
     
 }
