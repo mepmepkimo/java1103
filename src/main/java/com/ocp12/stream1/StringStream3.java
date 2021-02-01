d133ba54bcf769853b68c376a615ae98a6e946b0
mepmepkimo <lkjh855048>
2021-02-01 22:24:09 +0800

    Merge origin/master
    Conflicts:
    	src/main/java/practice2/ATM.java

--- src/main/java/com/ocp12/stream1/StringStream3.java
+++ src/main/java/com/ocp12/stream1/StringStream3.java
@@ -12,10 +12,15 @@ public class StringStream3 {
         System.out.println(sum);
         //印出字串長度<=4 的長度各是多少?
         Stream.of(names)//{"Java", "Python", "C", "Visual Basic"}
-                .mapToInt(x -> x.length())
-                .filter(x -> x <= 4)
-                .forEach(x->System.out.println(x));
+                .mapToInt(x -> x.length())//{4,6,1,12}
+                .filter(x -> x <= 4)//{4,1}
+                .forEach(x -> System.out.println(x));
         
+        
+        Stream.of(names)                          //{"Java", "Python", "C", "Visual Basic"}
+                .mapToInt(String::length)         //{4,6,1,12}
+                .filter(Util::isLessThan4)        //{4,1}
+                .forEach(System.out::println);
 
     }
 
---
+++ src/main/java/com/ocp12/stream1/Util.java
@@ -0,0 +1,9 @@
+
+package com.ocp12.stream1;
+
+public class Util {
+    public static boolean isLessThan4(int x) {
+        return x<=4;
+    }
+    
+}
